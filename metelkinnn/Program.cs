ff166db06afb82bc02d5c07c6e759d87865b2b22
metelkinn <152695621+metelkinn@users.noreply.github.com>
2024-06-16 14:57:17 +0300

    Add files via upload
    Добавлены необходимые сообщения

--- metelkinnn/Program.cs
+++ metelkinnn/Program.cs
@@ -4,9 +4,10 @@ class Program
 {
     static void Main()
     {
+
+        Console.Write("Введите число: ");
         double x = Convert.ToDouble(Console.ReadLine());
-
         double result = 2 * Math.Sin(x) + 3 * Math.Cos(x);
-
+        Console.WriteLine("Результат: " + result);
     }
 }
