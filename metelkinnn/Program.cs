cec957a996aa1b42a879724771b1167afe716b5d
женя <@METEKIN-ZHENYA->
2024-06-16 13:56:08 +0300

    Добавьте файлы проекта.

---
+++ metelkinnn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        double result = 2 * Math.Sin(x) + 3 * Math.Cos(x);
+
+    }
+}
